b72a64ec4a6dc6c963aed29e08e1abe324365810
vinay <vinaythapa762@gmail.com>
2024-08-27 10:43:34 +0545

    gemini chatbot

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+//import 'package:chatbot_app/constant/const.env';
+import 'package:chatbot_app/constant/const.dart';
+import 'package:chatbot_app/pages/chat_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+
+
+void main()async {
+  Gemini.init(apiKey:API_KEY );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatBot',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home:ChatPage()
+    );
+  }
+}
---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,88 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final Gemini gemini=Gemini.instance;
+  ChatUser currentUser= ChatUser(id: '0',firstName: 'see me');
+  ChatUser geminiUser=ChatUser(id:'1',firstName: 'Gemini',profileImage:  "https://seeklogo.com/images/G/google-gemini-logo-A5787B2669-seeklogo.com.png",);
+  List<ChatMessage> messages=[];
+  List<ChatUser> typingUser=<ChatUser> [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title:Text('ChatBot',
+        style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor:Colors.black
+      ),
+      backgroundColor: Colors.black45,
+      body: DashChat(
+        currentUser: currentUser,
+         onSend: sendMessage,
+          messages: messages,
+          typingUsers:typingUser,),
+    );
+  }
+  void sendMessage(ChatMessage m){
+    setState(() {
+    
+      messages=[m,...messages];
+      typingUser.add(geminiUser);
+    });
+    try{ List<Uint8List>? images;
+      if (m.medias?.isNotEmpty ?? false) {
+        images = [
+          File(m.medias!.first.url).readAsBytesSync(),
+        ];
+      }
+      String question=m.text;
+          gemini.streamGenerateContent(question).listen((event){
+            
+              ChatMessage? lastMessage = messages.firstOrNull;
+        if (lastMessage != null && lastMessage.user == geminiUser) {
+          lastMessage = messages.removeAt(0);
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
+          lastMessage.text += response;
+          setState(
+            () {
+              messages = [lastMessage!, ...messages];
+            },
+          );
+        } else {
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
+          ChatMessage message = ChatMessage(
+            user: geminiUser,
+            createdAt: DateTime.now(),
+            text: response,
+          );
+          setState(() {
+            messages = [message, ...messages];
+          });
+        }
+          });
+    }
+    catch(e){
+      print(e);
+    }
+    finally {
+    setState(() {
+      typingUser.remove(geminiUser);
+    });
+  }
+}}
